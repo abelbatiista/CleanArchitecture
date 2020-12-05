ccf1368d51ac9e6b09364e95f4b80d9b9ac6f996
ABel Batista Rodríguez <AbelBatiista@outlook.com>
2020-12-05 19:36:21 -0400

    CLEAN ARCHITECTURE

---
+++ CleanArchitecture.API/Controllers/PostController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchitecture.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetPost()
+        {
+            return Ok(null);
+        }
+    }
+}
